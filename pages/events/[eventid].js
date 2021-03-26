de53471e946d894a92198557fdf52cbe32d37d57
Matheus Lino <matheuslino86a@gmail.com>
2021-03-26 12:00:33 -0400

    event list ready

---
+++ pages/events/[eventid].js
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../dummy-data'
+
+function EventDetailPage(){
+    const router = useRouter()
+
+    const eventId = router.query.eventId
+    const event = getEventById(eventId)
+
+    if(!event){
+        return <p>No event found!</p>
+    }
+    
+    return(
+        <div>
+            <h1>Event Detail</h1>
+        </div>
+    )
+}
+
+export default EventDetailPage
